0acd86e32523663333646d66a6fe532ec9231731
FaithCoderLab <seinplan97@gmail.com>
2025-03-03 10:44:10 +0900

    initial commit

---
+++ src/main/java/com/example/myaccountsystem/MyAccountSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.myaccountsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAccountSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAccountSystemApplication.class, args);
+	}
+
+}
